757d65ec880328a3034b8e1a03b9397c6c84f3f5
AaronHale1989 <halea02130@gmail.com>
2018-06-12 13:32:06 -0400

    added new model and boat functonality

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,5 +1,6 @@
 class Boat < ApplicationRecord
-  has_many :jobs, :through => :users
+  has_many :jobs, :through => :schdules
+  has_many :schedules
   belongs_to :user
   validates :name, uniqueness: {case_sensitive: false}
 end
