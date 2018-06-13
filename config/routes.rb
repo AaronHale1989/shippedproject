09b5d00df54dcf74bdfd74fb849fae0608c55bcf
Jessica <jmc617@gmail.com>
2018-06-13 15:10:51 -0400

    logout fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
- 
+
   root "jobs#index"
 
   devise_for :users
-  
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   resources :jobs
   resources :users
   resources :boats
