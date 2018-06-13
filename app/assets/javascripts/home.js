44f69bea21e1e4b23cad517b033c37aa599981aa
AaronHale1989 <halea02130@gmail.com>
2018-06-13 14:17:42 -0400

    stuff

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -13,7 +13,7 @@ var start = Date.now();
  var timer = setInterval(function() {
   var timePassed = Date.now() - start;
 
-  boat.style.left = timePassed / 2 + 'px';
+  boat.style.left = timePassed / 5 + 'px';
 
   if (timePassed > 2000) clearInterval(timer);
 
